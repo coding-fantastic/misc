f95cdbbec2fbb5bd2cd056daa86a50c83986fc7b
coding-fantastic <62503960+coding-fantastic@users.noreply.github.com>
2020-09-16 12:32:03 +0300

    aar application form

---
+++ view.php
@@ -0,0 +1,696 @@
+
+<html>
+<head>
+    
+<!--    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    
+    
+    </head>
+
+<style>
+    body {
+        background: white !important;
+        color: black;
+        font-size: 15px
+    }
+
+    .box {
+        /*max-width: 800px;*/
+        margin: auto;
+    }
+
+    .area-padding {
+        padding: 10px
+    }
+
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5 {
+        color: #000;
+    }
+
+    .content {
+        border-bottom: solid 1px black;
+        border-bottom-style: dotted;
+    }
+
+    hr.bold {
+        border: 3px solid black;
+        margin: -13px 0 -13px 0
+    }
+
+    p {
+        color: black;
+    }
+
+    .center {
+        margin: auto;
+        padding: 10px;
+    }
+
+    .logo {
+        width: 100px;
+    }
+
+    .heading {
+        font-weight: 900;
+        margin-bottom: 10px;
+        padding-bottom: 0px;
+        color: #000;
+        font-size: 37px;
+        margin-left: -70px;
+    }
+
+    .box-height {
+        height: 120px;
+    }
+
+    strong {
+        font-size: 16px;
+    }
+    
+    .text-big{
+        font-size:20px;
+    }
+
+    .img {
+        max-height: 800px;
+        padding: 10px;
+    }
+
+    @media print {
+        .newPage {
+            page-break-after: always;
+        }
+
+        .box {
+            max-width: 100%;
+            margin: auto;
+        }
+
+        .img {
+            max-height: 800px;
+        }
+    }
+
+</style>
+  
+    
+    <style>
+    
+    .arrow-right {
+  background-color: #ff3300;
+  box-shadow: 0 0 3px 2px rgba(0,0,0,0.8);
+  height: 100px;
+  right: -50px;
+  position: absolute;
+  /*top: -50px;
+  width: 100px;
+  
+  -webkit-transform: rotate(-45deg);*/
+}
+    
+    </style>
+    
+    
+<body>
+    
+    <div class="container">
+
+        <div class="box area-padding">
+                
+            
+            
+            <div class="row mb-3">
+                <div class="col col-sm-10">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h2 class="text-center" style="color:red;" > Individual And Family Application Form </h2>
+            <h6 class="text-center" > (For membership to be considered this declaration mus be completed in full and all questions answered) </h6>
+                        </div>
+                        <!--<div class="col-sm-5 content text-center">
+                            <span>  <?php  echo "$our_ref";  ?></span>
+                        </div>-->
+                    </div>
+
+                </div>
+                <div class="col col-sm-2">
+                    
+                    <!--<img src="assets/images/aarlogo.png" alt="" class= "img-thumbnail">-->
+                    <div class="arrow-right"></div>            
+                </div>
+
+                
+            </div>
+            
+            
+            
+                <div class="arrow-right"></div>
+            
+            <h2 class="text-center" style="color:red;" > Individual And Family Application Form </h2>
+            <h6 class="text-center" > (For membership to be considered this declaration mus be completed in full and all questions answered) </h6>
+            
+            
+
+            <p class="text-left" style="color:#ff3300;">Principal Member Details</p>
+            <p class="text-right" style="color:#ff3300;">No. 11265</p>
+
+                
+            
+                
+            <div class="row mb-3">
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <p>Surname:</p>
+                        </div>
+                        <div class="col-sm-9 content text-center">
+                            <span>  </span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-5">
+                            <h6>First Name:</h6>
+                        </div>
+                        <div class="col-sm-7 content text-center">
+                            <span> <?php  echo "$your_ref";  ?></span>
+                        </div>
+                    </div>
+                </div>  
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-5">
+                            <h6>Middle Name:</h6>
+                        </div>
+                        <div class="col-sm-7 content text-center">
+                            <span>   </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+             <div class="row mb-3">
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-5">
+                            <p>Marital Status:</p>
+                        </div>
+                        <div class="col-sm-7 content text-center">
+                            <span>  <?php  echo "$our_ref";  ?></span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h6>Occupation:</h6>
+                        </div>
+                        <div class="col-sm-8 content text-center">
+                            <span> <?php  echo "$your_ref";  ?></span>
+                        </div>
+                    </div>
+                </div>  
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-7">
+                            <h6>PIN NO.(provide copy):</h6>
+                        </div>
+                        <div class="col-sm-5 content text-center">
+                            <span>   </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row mb-3">
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <p>Height:</p>
+                        </div>
+                        <div class="col-sm-9 content text-center">
+                            <span>  <?php  echo "$our_ref";  ?></span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6>Weight:</h6>
+                        </div>
+                        <div class="col-sm-9 content text-center">
+                            <span> <?php  echo "$your_ref";  ?></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                
+            
+            <div class="row mb-3">
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <p>ID/Passport No:</p>
+                        </div>
+                        <div class="col-sm-9 content text-center">
+                            <span>  <?php  echo "$our_ref";  ?></span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6>P.O. Box:</h6>
+                        </div>
+                        <div class="col-sm-9 content text-center">
+                            <span> <?php  echo "$your_ref";  ?></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-2">
+                            <p>Town:</p>
+                        </div>
+                        <div class="col-sm-10 content text-center">
+                            <span>  <?php  echo "$our_ref";  ?></span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6>Postal Code:</h6>
+                        </div>
+                        <div class="col-sm-9 content text-center">
+                            <span> <?php  echo "$your_ref";  ?></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row mb-3">
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-1">
+                            <p>Email:</p>
+                        </div>
+                        <div class="col-sm-11 content text-center">
+                            <span>  <?php  echo "$our_ref";  ?></span>
+                        </div>
+                    </div>
+
+                </div>
+
+                
+            </div>
+            
+            <div class="row mb-3">
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <p>Physical Address:</p>
+                        </div>
+                        <div class="col-sm-8 content text-center">
+                            <span>  <?php  echo "$our_ref";  ?></span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6>Mobile:</h6>
+                        </div>
+                        <div class="col-sm-9 content text-center">
+                            <span> <?php  echo "$your_ref";  ?></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <p class="text-left" style="color:#ff3300;">Dependants Details</p>    
+            
+            
+<table class="table table-bordered">
+<tr><p class="text-left" style="background-color:#ff3300; color:white; ">ENTER DETAILS OF THE SPOUSE (01) AND ALL DEPENDANTS TO BE INCLUDED IN THE  APPLICATION FOR MEMBERSHIP IN ORDER OF AGE (DESCENDING) WHERE APPLICABLE</p></tr>
+    <tr>
+    <th><small>Category</small></th>
+        <th><small>Surname</small></th>
+        <th><small>First Name</small></th>
+        <th><small>Middle Name</small></th>
+        <th  colspan="2"><small>Gender</small></th>
+        <th colspan="8"><small>Date of Birth</small></th>
+        <th><small>Height</small></th>
+        <th><small>Weight</small></th>
+    </tr>
+    
+    <tr>
+    <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th><small>M</small></th>
+        <th><small>F</small></th>
+        <th><small>D</small></th>
+        <th><small>D</small></th>
+        <th><small>M</small></th>
+        <th><small>M</small></th>
+        <th><small>Y</small></th>
+        <th><small>Y</small></th>
+        <th><small>Y</small></th>
+        <th><small>Y</small></th>
+        <th></th>
+        <th></th>
+    </tr>
+    <tr>
+    <th><small>01 Spouse</small></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+            <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </tr>
+    <tr>
+    <th><small>02 Dependant</small></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+            <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </tr>
+    <tr>
+    <th><small>03 Dependant</small></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+            <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </tr>
+    <tr>
+    <th><small>04 Dependant</small></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+            <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </tr>
+    <tr>
+    <th><small>05 Dependant</small></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+            <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </tr>
+    <tr>
+    <th><small>06 Dependant</small></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+            <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </tr>
+    <tr>
+    <th><small>07 Dependant</small></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+            <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </tr>
+            
+            </table>
+            
+            
+            
+            
+            <p class="text-left" style="color:#ff3300;">Next Of Kin Details(Beneficiary)</p>
+            <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th><small>Name</small></th>
+      <th><small>ID</small></th>
+      <th><small>Relationship</small></th>
+      <th><small>Phone Number</small></th>
+        
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+  </tbody>
+</table>
+    
+            
+            
+            
+            
+            <p class="text-left" style="color:#ff3300;">Cover Options</p>
+
+            
+<table class="table table-bordered">
+    
+    <tr>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th><small>Scope</small></th>
+        <th></th>
+        
+    </tr>
+    <tr>
+        <th><small style="color:#ff3300;" >Inpatient Cover Option</small></th>
+        <th><small>Platinum</small></th>
+        <th><small>Gold</small></th>
+        <th><small>Silver Plus</small></th>
+        <th><small>Silver</small></th>
+        <th><small>Bronze</small></th>
+        <th><small>Cover Me</small></th>
+        <th><small>Per Family</small></th>
+        <th></th>
+        
+    </tr>
+    <tr>
+        <th><small>(Tick Option)</small></th>
+        <th></th>
+        <th></th>    
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th><small>Per Person</small></th>
+        <th></th>
+        
+    </tr>
+    <tr>
+        <th><small style="color:#ff3300;">Outpatient Cover Option</small></th>
+        <th><small>250,000</small></th>
+        <th><small>200,000</small></th>    
+        <th><small>150,000</small></th>
+        <th><small>100,000</small></th>
+        <th><small>75,000</small></th>
+        <th><small>50,000</small></th>
+        
+    </tr>
+    <tr>
+        <th><small>(Tick Option)</small></th>
+        <th></th>
+        <th></th>    
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        
+    </tr>
+            
+            </table>            
+            
+            
+<table class="table table-bordered">
+            
+
+            </table>
+
+            <div class="row mb-3">
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-5">
+                            <p>Name of current/previous health insurer and the expiry date;</p>
+                        </div>
+                        <div class="col-sm-7 content text-center">
+                            <span>  <?php  echo "$our_ref";  ?></span>
+                        </div>
+                    </div>
+
+                </div>
+
+                
+            </div>
+            
+            <div class="row mb-3">
+                <div class="col">
+                    <div class="row">
+                        <div class="col-sm-5">
+                            <p>Previous Membership Number(if covered by AAR before);</p>
+                        </div>
+                        <div class="col-sm-7 content text-center">
+                            <span>  <?php  echo "$our_ref";  ?></span>
+                        </div>
+                    </div>
+
+                </div>
+
+                
+            </div>
+            
+            <div class="row mb-3">
+                <div class="col col-sm-10">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <p>Have you or any of your dependants ever been declined or premium loaded by any health insurer? State which one;</p>
+                        </div>
+                        <!--<div class="col-sm-5 content text-center">
+                            <span>  <?php  echo "$our_ref";  ?></span>
+                        </div>-->
+                    </div>
+
+                </div>
+                <div class="col col-sm-2">
+                    <img src="assets/images/aarlogo.png" alt="" class= "img-thumbnail">
+                </div>
+
+                
+            </div>
+            
+            
+            <div class="row mb-3">
+                <div class="col">
+                    <div class="row">
+                        
+                        <div class="col-sm-9 content text-center">
+                            <span>  <?php  echo "$our_ref";  ?></span>
+                        </div>
+                    </div>
+
+                </div>
+
+                
+            </div>
+            
+            
+            
+        </div>
+        
+    
+    </div>
+
+    
+    
+    
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    </body></html>
